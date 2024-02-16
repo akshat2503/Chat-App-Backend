4e58a1ac36cbe27e84cb7c4a383b4c6af3332faa
Akshat Gautam <ronitakki3@gmail.com>
2024-02-16 22:25:35 +0530

    Intermediate

--- index.js
+++ index.js
@@ -2,21 +2,18 @@ const express = require("express");
 const cors = require('cors');
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
-
+const connectDB = require("./config/db");
 
 const app = express();
 app.use(cors())
 dotenv.config();
+connectDB();
 
-app.get('/api/chats', (req, res)=> {
-    res.send(chats);
+app.get('/', (req, res)=> {
+    res.send("API running successfully.");
 });
 
-app.get('/api/chats/:id', (req, res)=> {
-    // res.send(chats);
-    const singleChat = chats.find((c)=>c._id === req.params.id);
-    res.send(singleChat);
-})
+app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
