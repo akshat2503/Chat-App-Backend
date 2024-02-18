b95d4720537191006ee8ebf6dd220b12db739996
Akshat Gautam <ronitakki3@gmail.com>
2024-02-18 21:37:54 +0530

    Coded the User Search API

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -46,7 +46,22 @@ const authUser = asyncHandler(async (req, res)=>{
             pic: user.pic,
             token: generateToken(user._id),
         });
+    } else {
+        res.status(401).json("Invalid Email or Password");
     }
 })
 
-module.exports = { registerUser, authUser };
+// /api/user?search=akshat
+const allUsers = asyncHandler(async (req, res)=>{
+    const keyword = req.query.search ? {
+        $or: [
+            { name: {$regex: req.query.search, $options: "i"} },
+            { email: {$regex: req.query.search, $options: "i"} },
+        ]
+    }:{};
+
+    const users = await User.find(keyword).find({_id:{$ne:req.user._id}});
+    res.send(users);
+})
+
+module.exports = { registerUser, authUser, allUsers };
